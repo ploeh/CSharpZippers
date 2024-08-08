6623dec676c8b5e043c2d870750e20c3443a1562
Mark Seemann <mark@ploeh.dk>
2024-08-08 15:59:36 +0200

    Add ListZipper class
    I'm doing test-driven development, and this commit is the result of the
    first red-green phases. As should be clear from both IDE suggestions and
    just the general state of the code, the refactoring phase still
    remains.

---
+++ ListZipper.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Ploeh.Samples.Zippers;
+
+internal class ListZipper<T>
+{
+    private int[] ints1;
+    private int[] ints2;
+
+    public ListZipper(int[] ints1, int[] ints2)
+    {
+        this.ints1 = ints1;
+        this.ints2 = ints2;
+    }
+
+    internal ListZipper<int> GoForward()
+    {
+        return new ListZipper<int>(new int[] { 2, 3, 4 }, new int[] { 1 });
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is ListZipper<T> other)
+            return ints1.SequenceEqual(other.ints1)
+                && ints2.SequenceEqual(other.ints2);
+
+        return base.Equals(obj);
+    }
+
+    public override string ToString()
+    {
+        return $"LZ ([{string.Join(", ", ints1)}], [{string.Join(", ", ints2)})]";
+    }
+}
---
+++ ListZipperTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+public sealed class ListZipperTests
+{
+    [Fact]
+    public void GoForward1()
+    {
+        var sut = new ListZipper<int>([1, 2, 3, 4], []);
+        var actual = sut.GoForward();
+        Assert.Equal(new ListZipper<int>([2, 3, 4], [1]), actual);
+    }
+}
