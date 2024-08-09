fa1b2a266a78a2b6b261856d7922b5a2553c8bb2
Mark Seemann <mark@ploeh.dk>
2024-08-09 09:34:49 +0200

    Add BinaryTree class
    As the astute reader will notice, this commit is accompanied by no
    tests. I didn't develop this class by TDD, but rather a kind of
    type-driven development based also on my knowledge of catamorphisms. The
    reader may want to consult
    https://blog.ploeh.dk/2019/06/10/tree-catamorphism for a similar
    example. This implementation, however, is much more locked down,
    indicating better encapsulation.

---
+++ BinaryTree.cs
@@ -0,0 +1,64 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+public sealed class BinaryTree<T>
+{
+    private readonly IBinaryTree root;
+
+    private BinaryTree(IBinaryTree root)
+    {
+        this.root = root;
+    }
+
+    public BinaryTree() : this(Empty.Instance)
+    {
+    }
+
+    public BinaryTree(T value, BinaryTree<T> left, BinaryTree<T> right)
+        : this(new Node(value, left.root, right.root))
+    {
+    }
+
+    TResult Aggregate<TResult>(
+        Func<TResult> whenEmpty,
+        Func<T, TResult, TResult, TResult> whenNode)
+    {
+        return root.Aggregate(whenEmpty, whenNode);
+    }
+
+    private interface IBinaryTree
+    {
+        TResult Aggregate<TResult>(
+            Func<TResult> whenEmpty,
+            Func<T, TResult, TResult, TResult> whenNode);
+    }
+
+    private sealed class Empty : IBinaryTree
+    {
+        public readonly static Empty Instance = new Empty();
+
+        private Empty()
+        {
+        }
+
+        public TResult Aggregate<TResult>(
+            Func<TResult> whenEmpty,
+            Func<T, TResult, TResult, TResult> whenNode)
+        {
+            return whenEmpty();
+        }
+    }
+
+    private sealed record Node(T Value, IBinaryTree Left, IBinaryTree Right)
+        : IBinaryTree
+    {
+        public TResult Aggregate<TResult>(
+            Func<TResult> whenEmpty,
+            Func<T, TResult, TResult, TResult> whenNode)
+        {
+            return whenNode(
+                Value,
+                Left.Aggregate(whenEmpty, whenNode),
+                Right.Aggregate(whenEmpty, whenNode));
+        }
+    }
+}
