319e47be31c262a745369084e03c0b5285e50de4
Mark Seemann <mark@ploeh.dk>
2024-08-12 07:58:02 +0200

    Add FSItem class

---
+++ FSItem.cs
@@ -0,0 +1,91 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+public sealed class FSItem
+{
+    private readonly IFSItem imp;
+
+    private FSItem(IFSItem imp)
+    {
+        this.imp = imp;
+    }
+
+    public static FSItem CreateFile(string name, string data)
+    {
+        return new(new File(name, data));
+    }
+
+    public static FSItem CreateFolder(
+        string name,
+        IReadOnlyCollection<FSItem> items)
+    {
+        return new(new Folder(name, items));
+    }
+
+    public TResult Aggregate<TResult>(
+        Func<string, string, TResult> whenFile,
+        Func<string, IReadOnlyCollection<TResult>, TResult> whenFolder)
+    {
+        return imp.Aggregate(whenFile, whenFolder);
+    }
+
+    private interface IFSItem
+    {
+        TResult Aggregate<TResult>(
+            Func<string, string, TResult> whenFile,
+            Func<string, IReadOnlyCollection<TResult>, TResult> whenFolder);
+    }
+
+    private sealed record File(string Name, string Data) : IFSItem
+    {
+        public TResult Aggregate<TResult>(
+            Func<string, string, TResult> whenFile,
+            Func<string, IReadOnlyCollection<TResult>, TResult> whenFolder)
+        {
+            return whenFile(Name, Data);
+        }
+    }
+
+    private sealed class Folder : IFSItem
+    {
+        private readonly string name;
+        private readonly IReadOnlyCollection<FSItem> items;
+
+        public Folder(string Name, IReadOnlyCollection<FSItem> Items)
+        {
+            name = Name;
+            items = Items;
+        }
+
+        public TResult Aggregate<TResult>(
+            Func<string, string, TResult> whenFile,
+            Func<string, IReadOnlyCollection<TResult>, TResult> whenFolder)
+        {
+            return whenFolder(
+                name,
+                items.Select(i => i.Aggregate(whenFile, whenFolder)).ToList());
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Folder folder &&
+                   name == folder.name &&
+                   items.SequenceEqual(folder.items);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(name, items);
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is FSItem item &&
+               EqualityComparer<IFSItem>.Default.Equals(imp, item.imp);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(imp);
+    }
+}
---
+++ FSItemTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+public sealed class FSItemTests
+{
+    [Fact]
+    public void CountFiles()
+    {
+        var sut = FSItem.CreateFolder("root",
+        [
+            FSItem.CreateFile("foo.txt", "Hello, world!"),
+            FSItem.CreateFolder("bar",
+            [
+                FSItem.CreateFile("baz.txt", "Goodbye, world!"),
+                FSItem.CreateFile("qux.txt", "Farewell, world!")
+            ])
+        ]);
+        var actual = sut.Aggregate(
+            whenFile: (_, _) => 1,
+            whenFolder: (_, counts) => counts.Sum());
+        Assert.Equal(3, actual);
+    }
+}
