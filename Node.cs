a2e9cd35c4b09db6cf5670c8ed488250144a5380
Mark Seemann <mark@ploeh.dk>
2024-08-12 16:16:46 +0200

    Add imperative doubly-linked node as example

---
+++ Node.cs
@@ -0,0 +1,15 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+// This class only exists to demonstrate how imperative code relies on state
+// mutation to implement a doubly-linked list.
+public sealed class Node<T>
+{
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; }
+    public Node<T>? Previous { get; set; }
+    public Node<T>? Next { get; set; }
+}
---
+++ NodeTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+public sealed class NodeTests
+{
+    [Fact]
+    public void Example()
+    {
+        var node1 = new Node<string>("foo");
+        var node2 = new Node<string>("bar") { Previous = node1 };
+        node1.Next = node2;
+
+        // Not much of an assertion, but I only add this test as a sanity check
+        // that I've implemented the Node class correctly.
+        Assert.Equal("bar", node1.Next.Value);
+        Assert.Equal("foo", node1.Next.Previous.Value);
+    }
+}
