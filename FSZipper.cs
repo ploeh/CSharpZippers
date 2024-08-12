5bf258f097b444053c1c3c6a3bbbd6ea31d99f1d
Mark Seemann <mark@ploeh.dk>
2024-08-12 08:17:57 +0200

    Add FSZipper class
    No behaviour yet.

---
+++ FSZipper.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Ploeh.Samples.Zippers;
+
+public sealed class FSZipper
+{
+    public FSZipper(FSItem fSItem, IReadOnlyCollection<FSCrumb> breadcrumbs)
+    {
+        FSItem = fSItem;
+        Breadcrumbs = breadcrumbs;
+    }
+
+    public FSItem FSItem { get; }
+    public IReadOnlyCollection<FSCrumb> Breadcrumbs { get; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is FSZipper zipper &&
+               FSItem.Equals(zipper.FSItem) &&
+               Breadcrumbs.SequenceEqual(zipper.Breadcrumbs);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FSItem, Breadcrumbs);
+    }
+}
