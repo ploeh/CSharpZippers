2262b86450a55b7771b82d2fa65a103e33198715
Mark Seemann <mark@ploeh.dk>
2024-08-12 08:08:57 +0200

    Add FSCrumb class

---
+++ FSCrumb.cs
@@ -0,0 +1,31 @@
+﻿namespace Ploeh.Samples.Zippers;
+
+public sealed class FSCrumb
+{
+    public FSCrumb(
+        string name,
+        IReadOnlyCollection<FSItem> left,
+        IReadOnlyCollection<FSItem> right)
+    {
+        Name = name;
+        Left = left;
+        Right = right;
+    }
+
+    public string Name { get; }
+    public IReadOnlyCollection<FSItem> Left { get; }
+    public IReadOnlyCollection<FSItem> Right { get; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is FSCrumb crumb &&
+               Name == crumb.Name &&
+               Left.SequenceEqual(crumb.Left) &&
+               Right.SequenceEqual(crumb.Right);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Left, Right);
+    }
+}
